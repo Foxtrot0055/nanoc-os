90ee08511a5a8fc0f98cc58575b03a5e5cb62fe4
Foxtrot0055 <bulldogge_komplex0q@icloud.com>
2025-01-08 20:33:52 +0100

    hello is at 0x7e00, zero buffer works confirmed isnt the issue tho

--- nanoc-bl.asm
+++ nanoc-bl.asm
@@ -2,50 +2,59 @@ bits 16
 org 0x7c00
 
 buffer:
-				dw 0x8000
-mov si, [buffer]
+	dw 0x8000
+xor di, di
 
 buffer_zero:
-				mov [si], 0
-				inc si
-				cmp si, buffer + 256
-				jne buffer_zero
+	mov word [di + 0x8000], 0
+	inc di
+	cmp di, 512
+	jne buffer_zero
 
 load_kernel:
         mov ah, 0x42 ;bios function for reading drive in lba(modern) mode
         mov dl, 0x80 ;set drive 0x80 which should be the c drive
-        int 0x13 ;bios int for function above
-        mov si, disk_adress_packet
-                
-disk_adress_packet:
-        db 0x10 ;size of dapack (16 bytes)
-        db 0 ;always zero
-        dw 16 ;read 16 packets for 8192 bytes 
-        dw [buffer] ;transfer buffer
-        dw 0x0003 ;transfer buffer offset (16byte steps)
-        dd hello ;low 32bits of starting lba 
-        dd 0 ;high 16bits of starting lba apperently it needs to be dd
-        
+        lea si, [disk_adress_packet]
+        mov di, 0 ;set di offset to 0
+        int 0x13 ;bios int for function above         
+       
 ;transition_to_protected_mode:
 ;        mov eax, cr0
 ;        mov eax, 1
 ;        mov cr0, eax
 
 mov si, 0
+
 print:
         mov ah, 0x0e
-        mov al, [$$ + si]
+        mov al, [0x8000 + si]
         int 0x10
         inc si
-        ;cmp byte [hello + si], 0
-        ;jne print
-        cmp si, 256
+        cmp si, 512
         jne print
-        jmp print - 1
+        mov al, byte "s" 
+        int 0x10
 
-				
-hlt
-end_label:
+lea ax, hello
+cmp ax, 0x7e00
+jne end
+
+checkmsg:
+        mov ah, 0x0e
+        mov al, byte "c"
+        int 0x10
+end:
+        hlt
+
+disk_adress_packet:
+        db 0x10 ;size of dapack (16 bytes)
+        db 0 ;always zero
+        dw 0x1 ;read 1 packets for 512 bytes 
+        dw 0x8000 ;transfer buffer start adress
+        dw 0x0000 ;transfer buffer offset (16byte steps)
+        dd 0X7e00 ;low 32bits of starting lba 
+        dd 0 ;high 16bits of starting lba apperently it needs to be dd
+
 times 510 - ($ - $$) db 0
 dw 0xAA55
 
