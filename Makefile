71c9ee74088468b991a547034e423ad4d1001eb1
Foxtrot0055 <bulldogge_komplex0q@icloud.com>
2025-04-15 12:42:50 +0200

    transition to protected mode works as well as basic VGA text

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 image:
 	dd if=nanoc-bl.bin of=nanoc.img
-	dd if=nanoc-ker.bin of=nanoc.img seek=1 bs=512
+	#dd if=nanoc-ker.bin of=nanoc.img seek=1 bs=512
 
 nanoc-bl: nanoc-bl.asm
 	nasm -f bin -o nanoc-bl.bin nanoc-bl.asm
--- nanoc-bl.asm
+++ nanoc-bl.asm
@@ -1,68 +1,81 @@
-bits 16
-org 0x7c00
+;bits 16
+[org 0x7c00]
 
-buffer:
-	dw 0x8000
+;buffer:
+;	dw 0x8000
 
 
-load_kernel:
-        xor di, di
-        mov ah, 0x42 ;bios function for reading drive in lba(modern) mode
-        mov dl, 0x80 ;set drive 0x80 which should be the c drive
-        lea si, [disk_adress_packet]
-        int 0x13 ;bios int for function above         
-       
-;transition_to_protected_mode:
-;        mov eax, cr0
-;        mov eax, 1
-;        mov cr0, eax
+;load_kernel:
+;        xor di, di
+;        mov ah, 0x42 ;bios function for reading drive in lba(modern) mode
+;        mov dl, 0x80 ;set drive 0x80 which should be the c drive
+;        lea si, [disk_adress_packet]
+;        int 0x13 ;bios int for function above         
 
-mov si, 0
-print:
-        mov ah, 0x0e
-        mov al, [0x8000 + si]
-        int 0x10
-        inc si
-        cmp si, 512
-        jne print
+;       mov si, 0
+;print:
+ ;       mov ah, 0x0e
+  ;      mov al, [0x8000 + si]
+   ;     int 0x10
+    ;    inc si
+     ;   cmp si, 512
+      ;  jne print
 
-hlt
 
-disk_adress_packet:
-        db 0x10     ;size of dapack (16 bytes)
-        db 0        ;always zero
-        dw 0x1      ;read 1 packets for 512 bytes 
-        dw 0x8000   ;transfer buffer start adress
-        dw 0x0000   ;transfer buffer offset (16byte steps)
-        dd 0X1      ;low 32bits of starting lba in 512byte units 
-        dd 0        ;high 16bits of starting lba apperently it needs to be dd
+CODE_SEG equ code_descriptor - GDT_Start
+DATA_SEG equ data_descriptor - GDT_Start
+
+
+transition_to_protected_mode:
+        cli
+        lgdt [GDT_Descriptor]
+        mov eax, cr0
+        or al, 1
+        mov cr0, eax
+        jmp CODE_SEG:start_protected_mode
+
+        jmp $
+        
+;disk_adress_packet:
+;        db 0x10     ;size of dapack (16 bytes)
+;        db 0        ;always zero
+;        dw 0x1      ;read 1 packets for 512 bytes 
+;        dw 0x8000   ;transfer buffer start adress
+;        dw 0x0000   ;transfer buffer offset (16byte steps)
+;        dd 0X1      ;low 32bits of starting lba in 512byte units 
+;        dd 0        ;high 16bits of starting lba apperently it needs to be dd
+
 
 GDT_Start:
         null_descriptor:
-                dd 0
-                dd 0
+                dd 0x0
+                dd 0x0
+                
         code_descriptor:
                 dw 0xFFFF; first 16bits of limit
-                dw 0x0000; first 24bits
-                db 0x00
-                db 10011010; access byte
-                db 11001111; xxxx=flags | xxxx=4bits of limit
-                db 0
+                dw 0x0; first 24bits
+                db 0x0
+                db 0b10011010; access byte
+                db 0b11001111; xxxx=flags | xxxx=4bits of limit
+                db 0x0
         data_descriptor:
                 dw 0xFFFF; first 16bits of limit
                 dw 0x0000; first 24bits
                 db 0x00
-                db 10010010; access byte
-                db 11001111; xxxx=flags | xxxx=4bits of limit
-                db 0
+                db 0b10010010; access byte
+                db 0b11001111; xxxx=flags | xxxx=4bits of limit
+                db 0x0
 GDT_End:
         
 GDT_Descriptor:
         dw GDT_End - GDT_Start - 1        ;size
         dd GDT_Start
 
-CODE_SEG equ code_descriptor - GDT_Start
-DATA_SEG equ data_descriptor - GDT_Start
+[bits 32]
+start_protected_mode:
+        mov al, 'A'
+        mov ah, 0x0f
+        mov [0xb8000], ax 
 
 times 510 - ($ - $$) db 0
 dw 0xAA55
