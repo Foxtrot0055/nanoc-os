283887fc30d0f5476969f129927e20b42f2beb68
Foxtrot0055 <bulldogge_komplex0q@icloud.com>
2025-04-15 18:44:16 +0200

    blinking issue solved, vga printing works well

--- nanoc-ker.asm
+++ nanoc-ker.asm
@@ -2,5 +2,12 @@
 [bits 32]
 
 mov al, 'B'
-mov ah, 0x11
-mov [0xb8000], ax
+mov ah, 0b00001111
+mov esi, 0
+mov ebx, 0xb8000
+loop_start:
+        mov [ebx + esi*2], ax
+        inc si
+        cmp si, 2000
+        jnz loop_start
+hlt
